918281d046ba2c0a173126beafabfccf9530869e
Everton Zanella <eza@protonmail.ch>
2019-10-15 12:27:13 +0200

    Simple space

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -6,7 +6,7 @@ const Suggestions = (props) => {
     const options = props.results.map(r => (
         
         <li key={r.id}>
-            <Highlight search={props.query}>{r.content}</Highlight><small>({moment(r.time).locale('PT_BR').format("L - H:mm")}) <a target="_blank" href={'https://twitter.com/jairbolsonaro/status/' + r.tweet.id_str}>link</a></small> <br/>
+            <Highlight search={props.query}>{r.content}</Highlight><small> ({moment(r.time).locale('PT_BR').format("L - H:mm")}) <a target="_blank" href={'https://twitter.com/jairbolsonaro/status/' + r.tweet.id_str}>link</a></small> <br/>
             ♻️ {r.tweet.retweet_count} ♥ {r.tweet.favorite_count}  
         </li>
     ));
