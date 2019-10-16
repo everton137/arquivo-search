dfcb03ce04340992b49ebcd8ca9ad18ed1ff99e1
Everton Zanella Alvarenga <eza@pm.me>
2019-10-16 10:41:23 +0200

    Date format change

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -6,7 +6,7 @@ const Suggestions = (props) => {
     const options = props.results.map(r => (
         
         <li key={r.id}>
-            <small>{moment(r.time).locale('PT_BR').format("L HH[h]mm")}</small>: <Highlight search={props.query}>{r.content}</Highlight><small> <a target="_blank" rel="noopener noreferrer" href={'https://twitter.com/jairbolsonaro/status/' + r.tweet.id_str}>tweet</a></small><br/>
+            <small>{moment(r.time).locale('PT_BR').format("DD/MM/YYYY HH[h]mm")}</small>: <Highlight search={props.query}>{r.content}</Highlight><small> <a target="_blank" rel="noopener noreferrer" href={'https://twitter.com/jairbolsonaro/status/' + r.tweet.id_str}>tweet</a></small><br/>
             ♻️ {r.tweet.retweet_count} ♥ {r.tweet.favorite_count}  
         </li>
     ));
