cd1b0f382f8ec81e5502600a9dd1f9dbd4cb6d5a
Everton Zanella Alvarenga <eza@pm.me>
2019-10-16 10:34:26 +0200

    Simple chane in the list

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -6,7 +6,7 @@ const Suggestions = (props) => {
     const options = props.results.map(r => (
         
         <li key={r.id}>
-            <Highlight search={props.query}>{r.content}</Highlight><small> ({moment(r.time).locale('PT_BR').format("L - H:mm")}) <a target="_blank" href={'https://twitter.com/jairbolsonaro/status/' + r.tweet.id_str}>link</a></small> <br/>
+            <small>{moment(r.time).locale('PT_BR').format("L HH[h]mm")}</small>: <Highlight search={props.query}>{r.content}</Highlight><small> <a target="_blank" rel="noopener noreferrer" href={'https://twitter.com/jairbolsonaro/status/' + r.tweet.id_str}>tweet</a></small><br/>
             ♻️ {r.tweet.retweet_count} ♥ {r.tweet.favorite_count}  
         </li>
     ));
