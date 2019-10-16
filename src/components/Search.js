67d9f2d7b2f46c5e21964c3aa0327a60a06b60e5
Everton Zanella Alvarenga <eza@pm.me>
2019-10-16 18:22:41 +0200

    Starting to test semantic-ui-react

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Suggestions from './Suggestions';
+import { Input } from 'semantic-ui-react';
 
 const API_URL = 'http://resistam.net:3001/tweets';
 
@@ -15,39 +16,42 @@ export default class Search extends Component {
 
     getInfo = () => {
         axios.get(`${API_URL}?content=ilike.*${this.state.query}*&order=time.desc`)
-            .then(({data}) => {
+            .then(({ data }) => {
                 // console.log(data)
                 this.setState({
                     results: data
                 })
             })
     };
-    
+
     handleInputChange = () => {
+        console.log(this.search.inputRef.current.value)
         this.setState({
-            query: this.search.value
+            query: this.search.inputRef.current.value
         }, () => {
             if (this.state.query && this.state.query.length > 1) {
                 if (this.state.query.length % 2 === 0) {
-                    this.getInfo()
+                    this.getInfo();
                 }
             } else if (!this.state.query) {
 
             }
         });
     }
-    
+
     render() {
-        return(
-            <form>
-                <input 
-                    placeholder="Buscar tweet..."
-                    ref={input => this.search = input}
-                    onChange={this.handleInputChange}
-                />
-                <p>Resultados: {this.state.results.length}</p>
-                <Suggestions results={this.state.results} query={this.state.query} /> 
-            </form>
+        return (
+                <form>
+                    <Input
+                        placeholder="Buscar tweet..."
+                        icon="search"
+                        ref={input => this.search = input}
+                        onChange={this.handleInputChange}
+                    />
+                    <p>Resultados: {this.state.results.length}</p>
+                    <Suggestions results={this.state.results} query={this.state.query} />
+                </form>
+
         );
     }
 }
